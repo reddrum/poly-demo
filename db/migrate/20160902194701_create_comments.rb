5d5ccda2c451fbf2a57eedd81d3fe2748e78e2c1
“reddrum” <anton.kranz@me.com>
2016-09-02 22:59:05 +0300

    Add post, images -> comment polymorphic associations

--- db/migrate/20160902194701_create_comments.rb
+++ db/migrate/20160902194701_create_comments.rb
@@ -2,7 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :content
-
+      t.references :comentable, polymorphic: true
+      
       t.timestamps null: false
     end
   end
